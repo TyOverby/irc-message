37495418c8c2d6fc6a8804702749562b32635b37
Ty Overby <ty@pre-alpha.com>
2014-08-03 22:43:46 -0700

    cleanup + comments

--- src/lib.rs
+++ src/lib.rs
@@ -30,6 +30,9 @@ fn find_after_n(string: &str, c: char, n: uint) -> Option<uint> {
 fn is_white(c: &char) -> bool { *c == ' ' }
 fn not_white(c: &char) -> bool { !is_white(c) }
 
+// Strips whitespace off an iterator.  It also recollects the
+// characters in order to keep rustc from blowing itself up.
+// github.com/rust-lang/rust/issues/16232
 fn strip<I: Iterator<char>>(iter: I) -> MoveItems<char> {
     let vec: Vec<char> = iter.skip_while(is_white).collect();
     vec.move_iter()
@@ -40,13 +43,17 @@ pub fn parse(line: &str) -> Result<IrcMessage, ()> {
     let mut chars = line.chars().peekable();
     // Tags
     if chars.peek() == Some(&'@') {
+        // Tags are from the @ to the first space character.
         let mut tags = chars.by_ref().skip(1).take_while(not_white);
         loop {
+            // Tags are seperated by semicolons
             let mut tag = tags.by_ref().take_while(|&c| c != ';').peekable();
             if tag.peek() == None { break; }
+            // A tag might have a '=' seperating key and value pairs
             let name = tag.by_ref().take_while(|&c| c != '=').collect();
             let mut rest = tag.peekable();
             let value = if rest.is_empty() {
+                // True is the default value if one is not provided.
                 "true".to_string()
             } else {
                 rest.collect()
@@ -54,9 +61,10 @@ pub fn parse(line: &str) -> Result<IrcMessage, ()> {
             message.tags.insert(name, value);
         }
     }
-    let mut chars = chars.peekable();
-    if chars.is_empty() { return Err(()); }
+
+    // An empty iterator at this point is a parsing failure
     let mut chars = strip(chars).peekable();
+    if chars.is_empty() { return Err(()); }
 
     // Prefix
     if chars.peek() == Some(&':') {
@@ -64,31 +72,26 @@ pub fn parse(line: &str) -> Result<IrcMessage, ()> {
         message.prefix = Some(prefix);
     }
 
-    let mut chars = chars.peekable();
+    let mut chars = strip(chars).peekable();
     if chars.is_empty() { return Err(()) }
-    let mut chars = strip(chars);
 
     // Command
     let command = chars.by_ref().take_while(not_white).collect();
     message.command = Some(command);
-    let mut chars = strip(chars);
+    let mut chars = strip(chars).peekable();
 
     // Params
     loop {
-        let mut chars = chars.by_ref().skip_while(is_white).peekable();
         match chars.peek() {
-            None => {
-                println!("breaking on none");
-                break; }
+            None => { break; }
             Some(&':') => {
+                // The rest of the string
                 message.params.push(chars.skip(1).collect());
-                println!("breaking on done");
                 break;
             }
             _ => {
-                println!("\nbefore");
-                chars.by_ref().inspect(|&c| print!("{}", c) );
                 message.params.push(chars.by_ref().take_while(not_white).collect());
+                chars = strip(chars).peekable();
             }
         }
     }
